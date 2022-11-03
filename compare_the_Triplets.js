065c9dee0b57a4e0c6e017cb376c364551825bdf
collinsmezie <collinschimezieobinwa@gmail.com>
2022-11-03 12:31:26 +0400

    problem pseudocode

--- compare_the_Triplets.js
+++ compare_the_Triplets.js
@@ -13,8 +13,23 @@ Category: Algorithms
 
 ------------PSEUDOCODE---------
 
+SET aliceInitialScore to 0
+SET bobInitialScore to 0
 
 
+FOR each score in Alice's score array
+
+  IF each score is greater than bob's score in the same position as Alice's THEN
+     INCREMENT aliceInitialScore
+
+  ELSE IF score is less than bob's score in the same position as Alice's THEN
+     INCREMENT bobInitialScore
+
+  END IF
+
+END FOR
+
+PRINT aliceInitialScore, bobInitialScore
 
 
 */
