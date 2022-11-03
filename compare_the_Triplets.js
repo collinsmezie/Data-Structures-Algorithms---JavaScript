c07d84bafd9835bb49fcd30fe5784ead19da13a0
collinsmezie <collinschimezieobinwa@gmail.com>
2022-11-03 12:38:03 +0400

    add code implementation

--- compare_the_Triplets.js
+++ compare_the_Triplets.js
@@ -19,10 +19,10 @@ SET bobInitialScore to 0
 
 FOR each score in Alice's score array
 
-  IF each score is greater than bob's score in the same position as Alice's THEN
+  IF score is greater than bob's score in the same array position as Alice's THEN
      INCREMENT aliceInitialScore
 
-  ELSE IF score is less than bob's score in the same position as Alice's THEN
+  ELSE IF score is less than bob's score in the same array position as Alice's THEN
      INCREMENT bobInitialScore
 
   END IF
@@ -37,17 +37,15 @@ PRINT aliceInitialScore, bobInitialScore
 
 
 
-
-
 //----------IMPLEMENTATION-------
 
-function compareTriplets(a, b){
-    let pointA = 0, pointB = 0;
-    for(let i = 0; i < a.length; i++){
+function compareTriplets(aliceArray, bobArray){
+    let aliceInitialScore = 0, bobInitialScore = 0;
+    for(let i = 0; i < aliceArray.length; i++){
   
-      (a[i] > b[i]) ? pointA += 1 : 
+      (aliceArray[i] > bobArray[i]) ? aliceInitialScore += 1 : 
       
-      (a[i] < b[i]) ? pointB += 1 : "continue"
+      (aliceArray[i] < bobArray[i]) ? bobInitialScore += 1 : "continue"
    }
-      return [pointA, pointB]
+      return [aliceInitialScore, bobInitialScore]
   }
